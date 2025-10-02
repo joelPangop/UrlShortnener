cb5de44eae8403a2edf7866aa80858a3cff0ae3f
joelpangop <joelpangopgmail.com>
2025-10-02 14:21:12 -0400

    Front end added

---
+++ UrlShortnener-client/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import UrlShortenerFormView from "./views/UrlShortenerFormView";
+
+function App() {
+    return <UrlShortenerFormView />;
+}
+
+export default App;
